e72bd91ac119e79b456cd46c4ef347d03d2d5765
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-07 15:07:50 +0000

    step 12 swapping partners

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,30 +1,32 @@
 require 'boris_bikes'
 
 describe DockingStation do
+
     describe ".release_bike" do
         it {is_expected.to respond_to(:release_bike)}
     end
-    context "bike working?" do
-      it "bike working" do
-        expect(subject.release_bike.working?).to eq(true)
+
+    describe "#release_bike" do
+      it "raises an error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error("Docking Station empty")
       end
     end
+
+    context "bike working?" do
+      it "bike working" do
+        expect(Bike.new.working?).to eq(true)
+      end
+    end
+
     describe ".dock" do
       it { is_expected.to respond_to(:dock).with(1).argument}
     end
 
-    it "returning bikes that are docked" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
+    describe '#release_bike' do
+      it "#release_bike" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
     end
 end
-    # describe ".release_bike" do
-    #     it {is_expected.to eq(Bike.new)}
-    # end
-    # describe ".working?" do
-    #     expect(DockingStation.new.).to respond_to(:working?)
-    # end
-    # describe ".working?" do
-    #     it {is_expected.to eq("working?")}
-    # end
